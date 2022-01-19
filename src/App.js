2cae764d746ad2bf8671fc3eec6dd2be38951b83
bavanadg <dhammikaedirisinghe@yahoo.com>
2022-01-19 23:08:36 +1030

    commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items:[],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        items: json,
+      })
+    });
+  }
+
+  render() {
+    var { isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return <div> Loading ...</div>
+}    
+    else {
+      return (
+        
+          <div className='App'  >
+            <header className='App-header'>
+              <h2> Contact Information</h2>
+            </header>
+            
+              <div>
+                 {items.map(item => (          
+                   <div className="user-container">
+                   <div className="info-item">
+                   <p> Name: {item.name} </p>
+                   <p> Email: {item.email} </p>
+                   <p className="txt">Address: {item.address.street}, {' '} 
+                   {item.address.suite},{' '}  
+                    {item.address.city},  {' '}
+                    {item.address.zipcode},  {' '}</p>
+                    <p>GEO Location : 
+                    lat: {item.address.geo.lat}{' '}
+                    lng: {item.address.geo.lng}
+
+                   </p>
+                   <p> Phone: {item.phone} </p>
+                   <p> Website: {item.website} </p>
+                   <p> Company: {item.company.name}</p>
+                   <p> CatchPhrase: {item.company.catchPhrase}</p>
+                   <p> BS: {item.company.bs}  </p>
+
+                   </div>
+                   </div>                  
+               ))}
+              </div> 
+
+                      
+          </div>    
+              
+    );
+        }
 }
-
+  
+}
 export default App;
